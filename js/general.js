60ccbf684f26a900a40d3d287f96e2c54e914583
Alex Ufiik <alexufiik@gmail.com>
2018-09-02 21:25:28 +0000

    jst

--- js/general.js
+++ js/general.js
@@ -1,24 +1,58 @@
 (function ($, undefined) {
   var $preloader = $('.preloader'),
-      $preloaderLogo = $('.preloader__logo');
+      $preloaderLogo = $('.preloader__logo'),
+      $preloaderSVG = $('.preloader__logo svg');
 
-  $preloaderLogo.show('slow' , function () {
-    $('.preloader')
-      .circleProgress(progressOptions)
-      .on('circle-animation-end', animationEndHandler)
+  $preloaderSVG.animate({
+    'opacity': 1,
+    'height': '59px',
+    'width': '57px'
+  }, {
+    'duration': 500,
+    'complete': prealoaderAnimation
   });
 
   var progressOptions = {
       value: 1,
       size: 150,
       fill: '#ffffff',
-      thickness: 1,
+      thickness: 1
       }
+
+  function scalledWrapper() {
+    var $wrapper = $('.wrapper');
+
+    (function () {
+      $wrapper
+        .removeClass('scld2 no-scroll')
+        .on('transitionend webkitTransitionEnd oTransitionEnd', function () {
+          $(document.body).removeClass('no-scroll');
+        })
+    }())
+  }
 
   function animationEndHandler() {
     $preloader.animate({
       'opacity': 'hide',
       'display': 'none'
-    })
+    }, scalledWrapper)
   }
+
+  function prealoaderAnimation() {
+    $('.preloader')
+      .circleProgress(progressOptions)
+      .on('circle-animation-end', animationEndHandler)
+  }
+  
+})(jQuery);
+
+
+// КОД ДЛЯ АНИМАЦИИ ПРЕЛОАДЕРА
+
+(function ($, undefined) {
+  var $hamburgerLink = $('.hamburger__link');
+
+  $hamburgerLink.on('click', function () {
+    alert(12321)
+  });
 })(jQuery);
